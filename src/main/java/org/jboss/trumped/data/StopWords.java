b5ff4838a7b98f7b0c575de4358bb0378fc2bd6a
George Gastaldi <gegastaldi@gmail.com>
2016-02-10 14:55:09 +0100

    Fixed things

--- src/main/java/org/jboss/trumped/data/StopWords.java
+++ src/main/java/org/jboss/trumped/data/StopWords.java
@@ -10,26 +10,29 @@ import java.util.List;
 import org.apache.lucene.analysis.util.CharArraySet;
 
 public class StopWords {
-	
-	public static final CharArraySet SET;
-	
-	static { 
-		try {
-			List<String> list = new ArrayList<String>();
-			String line;
-			try (InputStream fis = new FileInputStream("stop_words.txt");
-					InputStreamReader isr = new InputStreamReader(fis);
-					BufferedReader br = new BufferedReader(isr);) {
-				while ((line = br.readLine()) != null) {
-					if (!line.startsWith("#")) {
-						list.add(line);
+	private static CharArraySet SET;
+
+	public static CharArraySet getSet() {
+		if (SET == null) {
+			try {
+				List<String> list = new ArrayList<String>();
+				String line;
+				try (InputStream fis = Thread.currentThread().getContextClassLoader()
+						.getResourceAsStream("stop_words.txt");
+						InputStreamReader isr = new InputStreamReader(fis);
+						BufferedReader br = new BufferedReader(isr);) {
+					while ((line = br.readLine()) != null) {
+						if (!line.startsWith("#")) {
+							list.add(line);
+						}
 					}
 				}
+				SET = new CharArraySet(list, true);
+
+			} catch (Exception e) {
+				throw new RuntimeException("Problem while initializing Stopwords.LIST");
 			}
-			SET = new CharArraySet(list, true);
-		} catch (Exception e) {
-			throw new RuntimeException("Problem while initializing Stopwords.LIST");
 		}
+		return SET;
 	}
-	
 }
