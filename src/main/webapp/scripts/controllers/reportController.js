aae9edc100806d822e84fb9eebdfb09d7278eb83
George Gastaldi <gegastaldi@gmail.com>
2016-02-10 16:25:44 +0100

    Damn context path

--- src/main/webapp/scripts/controllers/reportController.js
+++ src/main/webapp/scripts/controllers/reportController.js
@@ -12,7 +12,7 @@ angular
 
 					$scope.mood = function(republican, democrat) {
 						var contextPath = $location.absUrl()
-								.contains('trumped/') ? '/trumped' : '';
+								.contains('http://trumped') ? '/' : '/trumped';
 						$scope.republicansImg = null;
 						$scope.democratsImg = null;
 						$scope.result = {};
