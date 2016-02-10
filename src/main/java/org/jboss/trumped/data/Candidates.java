402d5e1299e8fd0782d53dba29588d62d160edf8
George Gastaldi <gegastaldi@gmail.com>
2016-02-10 15:00:27 +0100

    Fixed typo

--- src/main/java/org/jboss/trumped/data/Candidates.java
+++ src/main/java/org/jboss/trumped/data/Candidates.java
@@ -58,7 +58,7 @@ public class Candidates {
 	    pages.add("http://www.nytimes.com/2016/02/10/us/politics/new-hampshire-primary.html");
 	    pages.add("http://www.theguardian.com/us-news/2016/feb/09/donald-trump-wins-new-hampshire-republican-primary-us-election-2016");
 	    pages.add("http://www.vox.com/2016/2/9/10955378/new-hampshire-primary-results-trump-wins");
-	    pages.add("http://edition.cnn.com/2016/02/09/politics/new-hampshire-primary-highlightsÒ");
+	    pages.add("http://edition.cnn.com/2016/02/09/politics/new-hampshire-primary-highlights");
 		LIST.put("Bernie Sanders", pages);
 		
 		pages = new ArrayList<String>();
--- src/main/webapp/scripts/controllers/reportController.js
+++ src/main/webapp/scripts/controllers/reportController.js
@@ -1,18 +1,23 @@
-angular.module('trumpedwebapp').controller(
-		'ReportController',
-		function($scope, $http, $location) {
-			$scope.republicans = [ "Donald Trump", "John Kasich", "Ted Cruz",
-					"Jeb Bush", "Marco Rubio", "Chris Christie",
-					"Carly Fiorina", "Ben Carson", "Jim Gilmore" ];
-			$scope.democrats = [ "Bernie Sanders", "Hillary Clinton",
-					"Martin O'Malley" ];
+angular.module('trumpedwebapp')
+		.controller(
+				'ReportController',
+				function($scope, $http, $location) {
+					$scope.republicans = [ "Donald Trump", "John Kasich",
+							"Ted Cruz", "Jeb Bush", "Marco Rubio",
+							"Chris Christie", "Carly Fiorina", "Ben Carson",
+							"Jim Gilmore" ];
+					$scope.democrats = [ "Bernie Sanders", "Hillary Clinton",
+							"Martin O'Malley" ];
 
-			$scope.mood = function(left, right) {
-				var contextPath = $location.absUrl().contains('trumped') ? '/trumped' : '';
-				$http.get(contextPath + '/rest/mood?democrat=' + left + '&republican=' + right)
-						.success(function(data) {
-							console.log(data);
-						})
-			}
+					$scope.mood = function(left, right) {
+						var contextPath = $location.absUrl()
+								.contains('trumped') ? '/trumped' : '';
+						$http.get(
+								contextPath + '/rest/mood?democrat=' + left
+										+ '&republican=' + right).success(
+								function(data) {
+									$scope.result = data;
+								})
+					}
 
-		});
+				});
