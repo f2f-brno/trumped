7effe8cc161013b89df527fc77474c3e3f199266
George Gastaldi <gegastaldi@gmail.com>
2016-02-10 14:31:09 +0100

    Combos ready

--- src/main/webapp/scripts/controllers/reportController.js
+++ src/main/webapp/scripts/controllers/reportController.js
@@ -1,5 +1,10 @@
-angular.module('trumpedwebapp').controller('ReportController',
+angular.module('trumpedwebapp').controller(
+		'ReportController',
 		function($scope, $http) {
-			console.log('I am here'); 
-		}
-);
+			$scope.republicans = [ "Donald Trump", "John Kasich", "Ted Cruz",
+					"Jeb Bush", "Marco Rubio", "Chris Christie",
+					"Carly Fiorina", "Ben Carson", "Jim Gilmore" ];
+			$scope.democrats = [ "Bernie Sanders", "Hillary Clinton",
+					"Martin O'Malley" ];
+
+		});
